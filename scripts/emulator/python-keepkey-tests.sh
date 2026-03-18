017a8686a53c6dd90c1c8c2a4216a943ae365f1e
highlander <highlander@keepkey.com>
2026-03-17 20:04:27 -0600

    fix(ci): stabilize solana python integration tests

--- scripts/emulator/python-keepkey-tests.sh
+++ scripts/emulator/python-keepkey-tests.sh
@@ -2,5 +2,8 @@
 
 mkdir -p /kkemu/test-reports/python-keepkey
 cd deps/python-keepkey/tests
-KK_TRANSPORT_MAIN=kkemu:11044 KK_TRANSPORT_DEBUG=kkemu:11045 pytest -v --junitxml=/kkemu/test-reports/python-keepkey/junit.xml
+PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION=python \
+KK_TRANSPORT_MAIN=kkemu:11044 \
+KK_TRANSPORT_DEBUG=kkemu:11045 \
+pytest -v --junitxml=/kkemu/test-reports/python-keepkey/junit.xml
 echo "$?" > /kkemu/test-reports/python-keepkey/status
