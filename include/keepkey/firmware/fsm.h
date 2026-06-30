03534e2231c90ef5097d6ac9490afda3920ffb76
highlander <highlander@keepkey.com>
2026-06-30 01:33:23 -0500

    style: clang-format-20

--- include/keepkey/firmware/fsm.h
+++ include/keepkey/firmware/fsm.h
@@ -142,6 +142,6 @@ void fsm_msgFlashWrite(FlashWrite* msg);
 void fsm_msgFlashHash(FlashHash* msg);
 void fsm_msgSoftReset(SoftReset* msg);
 
-void fsm_msgGetBip85Mnemonic(const GetBip85Mnemonic *msg);
+void fsm_msgGetBip85Mnemonic(const GetBip85Mnemonic* msg);
 
 #endif
