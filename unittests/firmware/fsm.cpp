2b7054cbb1d400a00e02b16141ab171cfce957db
highlander <highlander@keepkey.com>
2026-09-23 00:20:59 -0500

    fix(signing): renew idle deadline on accepted Bitcoin stages

--- unittests/firmware/fsm.cpp
+++ unittests/firmware/fsm.cpp
@@ -343,7 +343,6 @@ TEST_F(AutoLockProgress, IncompleteFrameCannotKeepStalledSigningUnlocked) {
   usb_test_receive(tail, sizeof(tail));
 }
 
-#if defined(KK_FINAL_POLICY_TESTS)
 TEST_F(AutoLockProgress, ValidBitcoinStreamProgressRenewsTheIdleDeadline) {
   TxAck ack = {};
   ack.has_tx = true;
@@ -372,7 +371,6 @@ TEST_F(AutoLockProgress, ValidBitcoinStreamProgressRenewsTheIdleDeadline) {
   EXPECT_FALSE(signing_is_active());
   EXPECT_EQ(SCREENSAVER, home_get_state());
 }
-#endif
 
 TEST_F(AutoLockProgress, FeaturePollingAtHomeDoesNotRenewTheIdleDeadline) {
   signing_abort();
